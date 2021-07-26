fb5b0bd0a3bc5ba00afa3aec3dfd431e893fc9b5
Srey1234 <74773452+Srey1234@users.noreply.github.com>
2021-07-26 19:27:21 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -15,6 +15,22 @@ function setup()
 function draw()
 {
     image ( video , 0 , 0 , 480 , 380 );
+    if(status != "")
+    {
+        objectDetector.detect( video , gotResults);
+          for (i = 0 ; i <object.length ; i++)
+          {
+              document.getElementById("status").innerHTML=" Status : Objects Dectected"
+              document.getElementById("number_of_object").innerHTML=" Number of object detected are : " + object.length;
+
+                fill( "#FF0000" );
+                 precent = floor ( objects[i].confidence * 100 ) ;
+                 text( object[i].label + " " + precent + " % " , object[i].x +15, object[i].y + 15 );
+                 oFill();
+                 stroke( "#FF0000" );
+                 rect (  object[i].x , object[i].y , object[i].width , object[i].height );
+          }
+    }
 }
 
 function start()
@@ -30,4 +46,14 @@ function modelloaded()
     video.loop();
     video.speed(1);
     video.volume(2);
+}
+
+function gotResults( error , results )
+{
+    if (error)
+    {
+        console.log(error);
+    }
+
+    console.log(results);
 }
